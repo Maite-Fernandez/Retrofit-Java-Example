b0b4fa3588bf0bff938db57c572b4905b82cdf6d
Maite Fernandez <maifermat.1997@gmail.com>
2020-03-30 11:46:54 +0200

    RetrofitJavaExample v2

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import okhttp3.*;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        HttpLoggingInterceptor interceptor= new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        GitHubService service = retrofit.create(GitHubService.class);
+
+        Call<List<Repo>> repos = service.listRepos("octocat");
+        try {
+            List<Repo> result = repos.execute().body();
+            for (Repo r : result) {
+                System.out.println(r);
+            }
+        }catch(Exception e){
+                System.out.println("EXEPTION");
+                System.out.println(e.toString());
+            }
+    }
+
+}
+
